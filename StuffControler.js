20f1cd129041bb004bebad047acba0fef5626f1a
Daniel Loto <Daniel@user-PC.ntmarein.local>
2018-01-06 11:02:18 +0700

    del extra files

--- StuffControler.js
+++
@@ -1,8 +0,0 @@
-var app = angular.module('myApp', []), d = new Date(), $mTahun, $gambarChart;
-app.controller("stuffControl", function($scope,$http) {
-	$http.get("StuffColectDataControl.php?q=").then(function (response) {$scope.customerData = response.data.records;});
-	$scope.fungsiSeekSquawk = function($tahun) {
-		$http.get("StuffColectDataControl.php?q="+ "ceding = '" + $tahun + "' ").then(function (response) {$scope.squawkData = response.data.records;});
-		document.getElementById("fourthChart").innerHTML = $gambarChart;
-	};	
-});
